4e8e54c3ed7c3b4b1c69f6fce6a43e75277c2c3d
isabe <is2430803@gmail.com>
2022-05-08 14:26:25 -0300

    first commit

---
+++ SegundoPrograma/src/br/md/Segundoprograma.java
@@ -0,0 +1,17 @@
+package br.md;
+
+public class Segundoprograma {
+
+public static <Gato, Livro> void main(String[] args) {
+   gato gato = new gato();
+   livro livro = new livro();
+
+		
+	System.out.println(gato);
+	System.out.println(livro);
+		
+		
+
+	}
+
+}
---
+++ SegundoPrograma/src/br/md/livro.java
@@ -0,0 +1,5 @@
+package br.md;
+
+public class livro {
+
+}
